60dcf0bca08ba54aae9a47638337a4f1ee9801a7
podorozniy <e.podorozniy@gmail.com>
2020-03-22 15:53:41 +0200

    single branch

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-git clone https://github.com/podorozniy/infra.git
+git clone --single-branch --branch podorozny-startup-script https://github.com/podorozniy/infra.git
 cd reddit && bundle install
 puma -d
