d4b57120ed3a54ca9e9ab822793ebd17b43f2171
podorozniy <e.podorozniy@gmail.com>
2020-03-22 17:39:30 +0200

    rvm+gem

--- 1install_ruby.sh
+++ 1install_ruby.sh
@@ -3,8 +3,8 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 curl -sSL https://get.rvm.io | bash -s stable
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
-cd ~/.rvm/scripts/rvm
-sudo rvm requirements
-sudo rvm install 2.4.1
-sudo rvm use 2.4.1 --default
-sudo gem install bundler -V --no-ri --no-rdoc
+cd ~/.rvm/scripts
+rvm requirements
+rvm install 2.4.1
+rvm use 2.4.1 --default
+gem install bundler -V --no-ri --no-rdoc
