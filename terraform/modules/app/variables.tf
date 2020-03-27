760f42c6787bb78b3ccfe5943f23d76f2c4b0e1b
e.podorozniy <e.podorozniy@unitsystems.com>
2020-03-27 18:07:42 +0200

    .gitignore appuser

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,8 @@
+variable public_key_path {
+  description = "Path to the public key used to connect to instance"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-app-base"
+}
