b7ea053f8c8215cc05daabae369baa4e5a570695
e.podorozniy <e.podorozniy@gmail.com>
2020-03-30 17:03:46 +0300

    ansible+terraform+packer

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,44 +11,32 @@ resource "google_compute_instance" "app" {
 
   # определение загрузочного диска
   boot_disk {
-    initialize_params {
-      image = "${var.disk_image}"
-    }
+  initialize_params {
+  image = "${var.disk_image}"
+  }
   }
 
   #ключ пользователя appuser в метаданных
   metadata = {
-    sshKeys = "appuser:${file("~/.ssh/appuser.pub")}"
+  sshKeys = "appuser:${file("~/.ssh/appuser.pub")}"
   }
 
   # определение сетевого интерфейса
   network_interface {
-    # сеть, к которой присоединить данный интерфейс
-    network = "default"
+  # сеть, к которой присоединить данный интерфейс
+  network = "default"
 
     # использовать ephemeral IP для доступа из Интернет
-    access_config {}
+  access_config {}
   }
 
   connection {
-    type        = "ssh"
-    user        = "appuser"
-    agent       = false
-    private_key = "${file("~/.ssh/appuser")}"
-  }
-
-  #копирование файла внутрь ВМ
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  #деплой приложения в ВМ
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
+   type        = "ssh"
+   user        = "appuser"
+   agent       = false
+   private_key = "${file("~/.ssh/appuser")}"
   }
 }
-
 resource "google_compute_firewall" "firewall_puma" {
   name = "allow-puma-default"
 
